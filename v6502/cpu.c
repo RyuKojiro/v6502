a91ac41930eeaafc09852c4d545f9ba89f79d2f5
Daniel Loffgren <daniel@loffgren.org>
2013-03-28 18:59:19 +0000

    Added cpu lifecycle code
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@4 96dfbd92-d197-e211-9ca9-00110a534b34

--- v6502/cpu.c
+++ v6502/cpu.c
@@ -6,4 +6,21 @@
 //  Copyright (c) 平成25年 Hello-Channel, LLC. All rights reserved.
 //
 
-#include <stdio.h>
+#include <stdlib.h>
+
+#include "cpu.h"
+#include "core.h"
+
+v6502_cpu *v6502_createCPU(void) {
+	// Allocate CPU Struct
+	v6502_cpu *cpu = malloc(sizeof(v6502_cpu));
+	if (!cpu) {
+		v6502_faultExternal("CPU Allocation - Internal Structure");
+		return NULL;
+	}
+	return cpu;
+}
+
+void v6502_destroyCPU(v6502_cpu *cpu) {
+	free(cpu);
+}
