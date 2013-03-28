076be98a8a9622adc9ec649ec8bafe52566af256
Daniel Loffgren <daniel@loffgren.org>
2013-03-28 18:59:26 +0000

    Added cpu lifecycle code
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@5 96dfbd92-d197-e211-9ca9-00110a534b34

--- v6502/cpu.h
+++ v6502/cpu.h
@@ -11,6 +11,35 @@
 
 #include <stdint.h>
 
-void v6502_execute(uint16_t instruction);
+#include "mem.h"
+
+typedef struct {
+	uint16_t pc;	// program counter
+	uint8_t ac;		// accumulator
+	uint8_t x;		// X register
+	uint8_t y;		// Y register
+	uint8_t sr;		// Status register
+	uint8_t sp;		// Stack Pointer
+	
+	v6502_memory *memory;
+} v6502_cpu;
+
+// CPU object lifecycle
+v6502_cpu *v6502_createCPU(void);
+void v6502_destroyCPU(v6502_cpu *cpu);
+
+// Execution
+void v6502_execute(v6502_cpu *cpu, uint16_t instruction);
+void v6502_step(v6502_cpu *cpu);
+
+// Instruction Transliteration
+const char *v6502_stringForInstruction(uint16_t instruction);
+uint16_t v6502_instructionForString(const char *string);
+
+// Instruction Set
+typedef enum {
+	v6502_opcode_brk = 0x0000,
+	//<#enumerator2#> = <#value2#>
+} v6502_opcode;
 
 #endif
