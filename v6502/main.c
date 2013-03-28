225ed70ce8cda3ba1922063e3bdb95204d30e853
Daniel Loffgren <daniel@loffgren.org>
2013-03-28 18:59:53 +0000

    Create a CPU and start REPLing
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@6 96dfbd92-d197-e211-9ca9-00110a534b34

--- v6502/main.c
+++ v6502/main.c
@@ -7,12 +7,29 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+
 #include "cpu.h"
 #include "mem.h"
+#include "core.h"
 
 int main(int argc, const char * argv[])
 {
+	printf("Creating 1 virtual CPU…\n");
+	v6502_cpu *cpu = v6502_createCPU();
+	
 	printf("Allocating virtual memory of size 2k…\n");
+	cpu->memory = v6502_createMemory(2048);
+	
+	char command[10];
+	for (;;) {
+		printf("] ");
+		scanf("%s", command);
+		
+		if (!strncmp(command, "!", 2)) {
+			v6502_printCpuState(cpu);
+		}
+	}
 	
     return 0;
 }
