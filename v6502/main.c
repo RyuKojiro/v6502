781cebe1805a2dcabe42f01055f1431bd6cbd8fa
Daniel Loffgren <daniel@loffgren.org>
2013-04-08 06:11:24 +0000

    The beginnings of enhanced capabilities of the interactive interpreter
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@78 96dfbd92-d197-e211-9ca9-00110a534b34

--- v6502/main.c
+++ v6502/main.c
@@ -9,6 +9,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
 
 #include "cpu.h"
 #include "mem.h"
@@ -28,6 +30,70 @@ static void popArg(char *str, size_t len) {
 	strncpy(str, space, MAX_COMMAND_LEN - diff);
 }
 
+static char getch() {
+	char buf = 0;
+	struct termios old = {0};
+	if (tcgetattr(0, &old) < 0)
+		perror("tcsetattr()");
+	old.c_lflag &= ~ICANON;
+	old.c_lflag &= ~ECHO;
+	old.c_cc[VMIN] = 1;
+	old.c_cc[VTIME] = 0;
+	if (tcsetattr(0, TCSANOW, &old) < 0)
+		perror("tcsetattr ICANON");
+	if (read(0, &buf, 1) < 0)
+		perror ("read()");
+	old.c_lflag |= ICANON;
+	old.c_lflag |= ECHO;
+	if (tcsetattr(0, TCSADRAIN, &old) < 0)
+		perror ("tcsetattr ~ICANON");
+	return (buf);
+}
+
+static void removeNewline(char *string) {
+	char *loc = strchr(string, '\n');
+	if (loc) {
+		*loc = '\0';
+	}
+}
+
+static void getInput(char *command, int len) {
+	char buf[len];
+	int x;
+	char in;
+	
+	removeNewline(command);
+	
+	for (x = 0; x < len && (buf[x - 1] != '\n'); x++) {
+		in = getch();
+		
+		if (in == '\357') {
+			x--;
+			if (getch() == '\234') {
+				switch (getch()) {
+					case '\200': { // UP
+						// TODO: Delete x chars
+						printf("%s", command);
+						fflush(stdout);
+						strncpy(buf + x, command, len - x);
+						x += (int)strlen(command);
+					} break;
+					case '\201': { // DOWN
+						// Nothing so far
+						// TODO: Delete x chars
+					} break;
+					default:
+						break;
+				}
+			}
+		}
+		else {
+			buf[x] = in;
+		}
+	}
+	strncpy(command, buf, x);
+}
+
 int main(int argc, const char * argv[])
 {
 	printf("Creating 1 virtual CPU…\n");
@@ -42,7 +108,10 @@ int main(int argc, const char * argv[])
 	char command[MAX_COMMAND_LEN];
 	for (;;) {
 		printf("(0x%x) ", cpu->pc);
-		fgets(command, sizeof(command), stdin);
+		fflush(stdout);
+		
+		// getInput(command, MAX_COMMAND_LEN);
+		fgets(command, MAX_COMMAND_LEN, stdin);
 		
 		if (command[0] == '\n') {
 			continue;
