ab411ddd35cdc0da1b619d75bbca948894ec69d2
Daniel Loffgren <daniel@loffgren.org>
2013-04-09 06:48:37 +0000

    Implemented BIT in parser
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@94 96dfbd92-d197-e211-9ca9-00110a534b34

--- v6502/parser.c
+++ v6502/parser.c
@@ -237,6 +237,16 @@ v6502_opcode v6502_opcodeForStringAndMode(const char *string, v6502_address_mode
 				return _opError(string, kBadAddressModeErrorText);
 		}
 	}
+	if (!strncmp(string, "bit", 3)) {
+		switch (mode) {
+			case v6502_address_mode_zeropage:
+				return v6502_opcode_bit_zpg;
+			case v6502_address_mode_absolute:
+				return v6502_opcode_bit_abs;
+			default:
+				return _opError(string, kBadAddressModeErrorText);
+		}
+	}
 	if (!strncmp(string, "cpx", 3)) {
 		switch (mode) {
 			case v6502_address_mode_immediate:
