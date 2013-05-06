b95cbe2d6570d51c182e42f5a69bf2a2a8f90222
Daniel Loffgren <daniel@loffgren.org>
2013-05-06 05:21:18 +0000

    Object source, with definitions
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@156 96dfbd92-d197-e211-9ca9-00110a534b34

---
+++ as6502/object.c
@@ -0,0 +1,57 @@
+//
+//  object.c
+//  v6502
+//
+//  Created by Daniel Loffgren on H.25/05/05.
+//  Copyright (c) 平成25年 Hello-Channel, LLC. All rights reserved.
+//
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "object.h"
+#include "common.h"
+
+// Object Lifecycle
+as6502_object *as6502_createObject() {
+	as6502_object *obj = malloc(sizeof(as6502_object));
+	
+	obj->count = 0;
+	obj->blobs = NULL;
+	
+	return obj;
+}
+
+void as6502_destroyObject(as6502_object *obj) {
+	for (int i = 0; i < obj->count; i++) {
+		free(obj->blobs[i].data);
+	}
+	free(obj->blobs);
+	free(obj);
+}
+
+// Object Accessors
+void as6502_writeObjectToFile(as6502_object *obj, FILE *file) {
+	// TODO: object file writeout
+}
+
+void as6502_addBlobToObject(as6502_object *obj, uint16_t start, uint16_t len, uint8_t *data) {
+	size_t size = sizeof(uint8_t) * len;
+	uint8_t *newData = malloc(size);
+	if (!newData) {
+		die("data malloc in as6502_addBlobToObject");
+	}
+	
+	memcpy(newData, data, size);
+	
+	obj->blobs = realloc(obj->blobs, sizeof(uint8_t *) * (obj->count + 1));
+	if (!obj->blobs) {
+		die("blobs realloc in as6502_addBlobToObject");
+	}
+	
+	obj->blobs[obj->count].data = newData;
+	obj->blobs[obj->count].start = start;
+	obj->blobs[obj->count].len = len;
+	
+	obj->count++;
+}
