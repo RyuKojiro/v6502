c8307a4b79aaf2bae29d217b162b6320bdcc3dfc
Daniel Loffgren <daniel@loffgren.org>
2013-05-06 05:12:48 +0000

    Added a common code file for things like die()
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@155 96dfbd92-d197-e211-9ca9-00110a534b34

---
+++ as6502/common.h
@@ -0,0 +1,14 @@
+//
+//  common.h
+//  v6502
+//
+//  Created by Daniel Loffgren on H.25/05/05.
+//  Copyright (c) 平成25年 Hello-Channel, LLC. All rights reserved.
+//
+
+#ifndef v6502_common_h
+#define v6502_common_h
+
+void die(const char *reason);
+
+#endif
