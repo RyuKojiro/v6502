02ebce15cbe19d9c077b72dda5060286fa73e0f1
Daniel Loffgren <daniel@loffgren.org>
2013-05-06 02:42:17 +0000

    labels now manages all symbols
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@149 96dfbd92-d197-e211-9ca9-00110a534b34

--- as6502/labels.h
+++ as6502/labels.h
@@ -6,26 +6,41 @@
 //  Copyright (c) 平成25年 Hello-Channel, LLC. All rights reserved.
 //
 
-#ifndef v6502_labels_h
-#define v6502_labels_h
+#ifndef as6502_labels_h
+#define as6502_labels_h
 
 #include <stdint.h>
 #include <stdio.h>
 
 // Types
+
 typedef struct {
-	int size;
-	uint16_t addresses[];
-} as6502_label_table;
+	char *name;
+	uint16_t address;
+} as6502_label;
+
+typedef struct {
+	char *name;
+	uint16_t address;
+} as6502_var;
+
+typedef struct {
+	int labelCount;
+	as6502_label *labels;
+	int varCount;
+	as6502_var *vars;
+} as6502_symbol_table;
 
 // Address Table Lifecycle Functions
-as6502_label_table *as6502_createLabelTable();
-void as6502_destroyLabelTable(as6502_label_table *table);
-void as6502_populateLabelTableWithFile(as6502_label_table *table, FILE *file);
-void as6502_printLabelTable(as6502_label_table *table);
+as6502_symbol_table *as6502_createLabelTable();
+void as6502_destroySymbolTable(as6502_symbol_table *table);
+void as6502_populatSymbolTableWithFile(as6502_symbol_table *table, FILE *file);
+void as6502_printSymbolTable(as6502_symbol_table *table);
 
-// Address Table Accessors
-void as6502_addLabelToTable(as6502_label_table *table, const char labelName, uint16_t address);
-uint16_t as6502_addressForLabel(as6502_label_table *table, const char labelName);
+// Symbol Table Accessors
+void as6502_addLabelToTable(as6502_symbol_table *table, const char name, uint16_t address);
+uint16_t as6502_addressForLabel(as6502_symbol_table *table, const char name);
+void as6502_addVarToTable(as6502_symbol_table *table, const char name, uint16_t address);
+uint16_t as6502_addressForVar(as6502_symbol_table *table, const char name);
 
 #endif
