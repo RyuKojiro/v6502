d121343d7cd5cda67bb17889c3216b9eb9635e54
Daniel Loffgren <daniel@loffgren.org>
2013-05-30 22:33:50 +0000

    Fully doxygenned object header
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@194 96dfbd92-d197-e211-9ca9-00110a534b34

--- as6502/object.h
+++ as6502/object.h
@@ -15,36 +15,56 @@
 /** @struct */
 /** @brief A single blob of typeless object data */
 typedef struct {
+	/** @brief The start address of the data */
 	uint16_t start;
+	/** @brief The length of the data */
 	uint16_t len;
+	/** @brief The data */
 	uint8_t *data;
 } as6502_object_blob;
 
 /** @struct */
 /** @brief The assembler's representation of an object file in memory */
 typedef struct {
+	/** @brief The number of as6502_object_blob's */
 	int count;
+	/** @brief An array of as6502_object_blob's */
 	as6502_object_blob *blobs;
 } as6502_object;
 
 /** @struct */
 /** @brief A running context that can be kept to build an object file procedurally */
 typedef struct {
+	/** @brief The as6502_object that a context refers to */
 	as6502_object *obj;
+	/** @brief The current blob in obj->blobs */
 	int currentBlob;
 } as6502_object_context;
 
-// Object Lifecycle
+/** @defgroup obj_lifecycle Object Lifecycle Functions */
+/**@{*/
+/** @brief Creates a new as6502_object */
 as6502_object *as6502_createObject();
+/** @brief Destroys an as6502_object */
 void as6502_destroyObject(as6502_object *obj);
+/** @brief Creates a new as6502_object_context */
 as6502_object_context *as6502_createObjectContext();
+/** @brief Destroys an as6502_object_context */
 void as6502_destroyObjectContext(as6502_object_context *ctx);
+/**@}*/
 
-// Object Accessors
+/** @defgroup obj_access Object Accessors */
+/**@{*/
+/** @brief Writes an as6502_object directly to a file handle */
 void as6502_writeObjectToFile(as6502_object *obj, FILE *file);
+/** @brief Creates an as6502_object_blob and adds it to an as6502_object */
 void as6502_addBlobToObject(as6502_object *obj, uint16_t start, uint16_t len, uint8_t *data);
+/**@}*/
 
-// Contextual Object Mutators
+/** @defgroup obj_access Contextual Object Mutators */
+/**@{*/
+/** @brief Automatically processes any dot directive in a given line and updates an as6502_object_context */
 void as6502_processObjectDirectiveForLine(as6502_object_context *ctx, const char *line, size_t len);
+/**@}*/
 
 #endif
