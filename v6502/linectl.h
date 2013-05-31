33d728f8cbf52af6a1540cc0551aea762ce1252d
Daniel Loffgren <daniel@loffgren.org>
2013-05-31 01:35:57 +0000

    Forgot to update @defgroup
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@203 96dfbd92-d197-e211-9ca9-00110a534b34

--- v6502/linectl.h
+++ v6502/linectl.h
@@ -11,7 +11,7 @@
 
 #include <sys/types.h>
 
-/** @defgroup mem_access Memory Access */
+/** @defgroup linectl Line Manipulation Functions */
 /**@{*/
 /** @brief Destructively trim trailing whitespace with NUL */
 void trimtaild(char *str);
