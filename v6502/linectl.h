3a633b4e60a23b8d1fa44fb60e34c4f5056d3b15
Daniel Loffgren <daniel@loffgren.org>
2013-05-31 01:35:10 +0000

    linectl doxygen
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@202 96dfbd92-d197-e211-9ca9-00110a534b34

--- v6502/linectl.h
+++ v6502/linectl.h
@@ -11,31 +11,34 @@
 
 #include <sys/types.h>
 
-// Destructively trim trailing whitespace with NUL
+/** @defgroup mem_access Memory Access */
+/**@{*/
+/** @brief Destructively trim trailing whitespace with NUL */
 void trimtaild(char *str);
 
-// Destructively tail trim at first encounter of token char from the tail end
+/** @brief Destructively tail trim at first encounter of token char from the tail end */
 void trimtailchard(char *str, char token);
 
-// Destructively tail trim at first encounter of token char from the head end
+/** @brief Destructively tail trim at first encounter of token char from the head end */
 void trimgreedytaild(char *str);
 
-// Destructively tail trim at first encounter of token char from the head end
+/** @brief Destructively tail trim at first encounter of token char from the head end */
 void trimgreedytailchard(char *str, char token);
 
-// Safely trim leading whitespace by pushing pointer
+/** @brief Safely trim leading whitespace by pushing pointer */
 char *trimhead(char *str);
 
-// Safely trim head til first encounter of token char from the head end
+/** @brief Safely trim head til first encounter of token char from the head end */
 char *trimheadchar(char *str, char token);
 
-// Reverse search string for space, safely
+/** @brief Reverse search string for space, safely */
 char *rev_strnspc(char *str, char *start);
 
-// Reverse search string for character, safely
+/** @brief Reverse search string for character, safely */
 char *rev_strnchr(char *str, char *start, char chr);
 
-// Safely search potentially unterminated string for character
+/** @brief Safely search potentially unterminated string for character */
 char *strnchr(char *str, char chr, size_t len);
+/**@}*/
 
 #endif
