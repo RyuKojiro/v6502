d4221361e746fa2f6081ee7614ec42cae4bb1b0b
Daniel Loffgren <daniel@loffgren.org>
2013-06-03 21:42:03 +0000

    Simplest fault tolerant vm
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@261 96dfbd92-d197-e211-9ca9-00110a534b34

--- easy6502/main.c
+++ easy6502/main.c
@@ -6,12 +6,40 @@
 //  Copyright (c) 平成25年 Hello-Channel, LLC. All rights reserved.
 //
 
-#include <stdio.h>
 #include "cpu.h"
 #include "mem.h"
 
+#include "video.h"
+
+/*
+ Notes:
+ 
+ Memory location $fe contains a new random byte on every instruction.
+ Memory location $ff contains the ascii code of the last key pressed.
+ 
+ Memory locations $200 to $5ff map to the screen pixels. Different values will
+ draw different colour pixels. The colours are:
+ */
+
+static int faulted;
+
+void v6502_fault(const char *e) {
+	faulted++;
+}
+
 int main(int argc, const char * argv[])
 {
+	v6502_cpu *cpu = v6502_createCPU();
+	cpu->memory = v6502_createMemory(2048);
 	
+	v6502_reset(cpu);
+	
+	while (!faulted) {
+		v6502_step(cpu);
+		updateVideo(cpu->memory);
+	}
+
+	v6502_destroyMemory(cpu->memory);
+	v6502_destroyCPU(cpu);
 }
 
