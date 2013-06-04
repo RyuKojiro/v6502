e4f61459a745682d70baf4f8e939908d5ef9a6c6
Daniel Loffgren <daniel@loffgren.org>
2013-06-04 20:34:33 +0000

    Doxygen for reverse.h
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@275 96dfbd92-d197-e211-9ca9-00110a534b34

--- as6502/reverse.h
+++ as6502/reverse.h
@@ -1,6 +1,6 @@
 //
-//  reverse.h
-//  v6502
+/** @brief Instruction Disassembly */
+/** @file reverse.h */
 //
 //  Created by Daniel Loffgren on H.25/06/04.
 //  Copyright (c) 平成25年 Hello-Channel, LLC. All rights reserved.
@@ -14,8 +14,14 @@
 #include "cpu.h"
 #include "parser.h"
 
+/** @defgroup rev Instruction Disassembly */
+/**@{*/
+/** @brief Get the string representation of an opcode */
 void as6502_stringForOpcode(char *string, size_t len, v6502_opcode opcode);
+/** @brief Get the string representation of the operands in a given address mode */
 void as6502_stringForOperand(char *string, size_t len, as6502_address_mode opcode, uint8_t high, uint8_t low);
+/** @brief Get the complete string representation of an instruction line */
 void as6502_stringForInstruction(char *string, size_t len, v6502_opcode opcode, uint8_t high, uint8_t low);
+/**@}*/
 
 #endif
