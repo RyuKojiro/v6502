90dc066632a3ea72c657c6fc638bb8d4c36738c4
Daniel Loffgren <daniel@loffgren.org>
2013-06-10 00:42:53 +0000

    Implemented the decimal portion of +setColorForByte
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@290 96dfbd92-d197-e211-9ca9-00110a534b34

--- cocoa-easy6502/cocoa-easy6502/VMVideoView.m
+++ cocoa-easy6502/cocoa-easy6502/VMVideoView.m
@@ -23,6 +23,66 @@
     return self;
 }
 
+/*
+ Memory locations $200 to $5ff map to the screen pixels. Different values will
+ draw different colour pixels. The colours are:
+ 
+ $0: Black
+ $1: White
+ $2: Red
+ $3: Cyan
+ $4: Purple
+ $5: Green
+ $6: Blue
+ $7: Yellow
+ $8: Orange
+ $9: Brown
+ $a: Light red
+ $b: Dark grey
+ $c: Grey
+ $d: Light green
+ $e: Light blue
+ $f: Light grey
+ */
+
++ (void)setColorForByte:(uint8_t)byte {
+	byte &= 0xf;
+	switch (byte) {
+		case 0: {
+			[[NSColor blackColor] setFill];
+		} return;
+		case 1: {
+			[[NSColor whiteColor] setFill];
+		} return;
+		case 2: {
+			[[NSColor redColor] setFill];
+		} return;
+		case 3: {
+			[[NSColor cyanColor] setFill];
+		} return;
+		case 4: {
+			[[NSColor purpleColor] setFill];
+		} return;
+		case 5: {
+			[[NSColor greenColor] setFill];
+		} return;
+		case 6: {
+			[[NSColor blueColor] setFill];
+		} return;
+		case 7: {
+			[[NSColor yellowColor] setFill];
+		} return;
+		case 8: {
+			[[NSColor orangeColor] setFill];
+		} return;
+		case 9: {
+			[[NSColor brownColor] setFill];
+		} return;
+		default:
+			break;
+	}
+}
+
 - (void)drawRect:(NSRect)dirtyRect {
     // set any NSColor for filling, say white:
     [[NSColor blackColor] setFill];
@@ -39,6 +99,7 @@
 		for (int y = 0; y < dirtyRect.size.height; y++) {
 			byte = mem->bytes[x * (int)dirtyRect.size.width + y + VIDEO_OFFSET];
 			if (byte) {
+				[VMVideoView setColorForByte:byte];
 				NSRectFill(NSMakeRect(x * scale, y * scale, scale, scale));
 			}
 		}
