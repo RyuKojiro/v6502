365a3ce2aa6391bf5528492b957296920d56d325
Daniel Loffgren <daniel@loffgren.org>
2013-06-17 22:20:06 +0000

    Error doxygen
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@318 96dfbd92-d197-e211-9ca9-00110a534b34

--- as6502/error.h
+++ as6502/error.h
@@ -1,5 +1,6 @@
 //
-//  error.h
+/** @brief Assembler error handling */
+/** @file error.h */
 //  v6502
 //
 //  Created by Daniel Loffgren on H.25/05/05.
@@ -12,8 +13,14 @@
 extern unsigned long currentLineNum;
 extern const char *currentFileName;
 
+/** @defgroup as_error Assembler Error Handling */
+/**@{*/
+/** @brief Called for fatal errors during assembly, such as internal memory failures */
 __attribute((noreturn)) void as6502_fatal(const char *reason);
+/** @brief Called for code errors detected during assembly, which prevent producing a binary */
 void as6502_error(const char *error);
+/** @brief Called for code warnings detected during assembly, which don't prevent producing a binary */
 void as6502_warn(const char *warning);
+/**@}*/
 
 #endif
