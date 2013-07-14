771de645cc2cb705cc416c2a36b9fe2bb33825e3
Daniel Loffgren <daniel@loffgren.org>
2013-07-14 02:58:05 +0000

    Removed all of the deprecated (and problematic) termios code
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@377 96dfbd92-d197-e211-9ca9-00110a534b34

--- v6502/main.c
+++ v6502/main.c
@@ -10,12 +10,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <termios.h>
 
 #include "cpu.h"
 #include "mem.h"
 #include "core.h"
 #include "parser.h"
+#include "error.h"
 
 #define MAX_COMMAND_LEN		80
 
@@ -38,72 +38,10 @@ static void popArg(char *str, size_t len) {
 	strncpy(str, space, MAX_COMMAND_LEN - diff);
 }
 
-static char getch() {
-	char buf = 0;
-	struct termios old = {0};
-	if (tcgetattr(0, &old) < 0)
-		perror("tcsetattr()");
-	old.c_lflag &= ~ICANON;
-	old.c_lflag &= ~ECHO;
-	old.c_cc[VMIN] = 1;
-	old.c_cc[VTIME] = 0;
-	if (tcsetattr(0, TCSANOW, &old) < 0)
-		perror("tcsetattr ICANON");
-	if (read(0, &buf, 1) < 0)
-		perror ("read()");
-	old.c_lflag |= ICANON;
-	old.c_lflag |= ECHO;
-	if (tcsetattr(0, TCSADRAIN, &old) < 0)
-		perror ("tcsetattr ~ICANON");
-	return (buf);
-}
-
-static void removeNewline(char *string) {
-	char *loc = strchr(string, '\n');
-	if (loc) {
-		*loc = '\0';
-	}
-}
-
-static void getInput(char *command, int len) {
-	char buf[len];
-	unsigned int x;
-	char in;
-	
-	removeNewline(command);
-	
-	for (x = 0; (x < len) && (buf[x - 1] != '\n'); x++) {
-		in = getch();
-		
-		if (in == '\357') {
-			x--;
-			if (getch() == '\234') {
-				switch (getch()) {
-					case '\200': { // UP
-						/** TODO: @todo Delete x chars */
-						printf("%s", command);
-						fflush(stdout);
-						strncpy(buf + x, command, len - x);
-						x += (int)strlen(command);
-					} break;
-					case '\201': { // DOWN
-						// Nothing so far
-						/** TODO: @todo Delete x chars */
-					} break;
-					default:
-						break;
-				}
-			}
-		}
-		else {
-			buf[x] = in;
-		}
-	}
-	strncpy(command, buf, x);
-}
-
 int main(int argc, const char * argv[])
 {
+	currentFileName = "v6502";
+	
 	printf("Creating 1 virtual CPU…\n");
 	v6502_cpu *cpu = v6502_createCPU();
 	cpu->fault_callback = fault;
