b7c3943f1c118a4c4cdbb00deba0cabf0267bc4d
Daniel Loffgren <daniel@loffgren.org>
2013-07-17 16:29:13 +0000

    Removed preceding exclamation point from commands, and added a function to allow short commands
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@394 96dfbd92-d197-e211-9ca9-00110a534b34

--- v6502/main.c
+++ v6502/main.c
@@ -105,49 +105,58 @@ static void run(v6502_cpu *cpu) {
 	}
 }
 
-/** 0 is success, 1 is exit */
+static int compareCommand(const char * restrict command, const char * restrict literal) {
+	size_t len = strlen(literal);
+	for (size_t i = 0; i < len; i++) {
+		if (command[i] != literal[i] && command[i] != '\n' && command[i] != '\0') {
+			return NO;
+		}
+	}
+	return YES;
+}
+
+/** return YES if handled */
 static int handleDebugCommand(v6502_cpu *cpu, char *command) {
-	if (!strncmp(command, "help", 4)) {
-		printf("!cpu         Displays the current state of the CPU.\n"
-			   "!dis <addr>  Disassemble ten instructions starting at a given address, or the program counter if no address is specified.\n"
-			   "!help        Displays this help.\n"
-			   "!load <file> Load binary image into memory at 0x0600.\n"
-			   "!peek <addr> Dumps the memory at and around a given address.\n"
-			   "!quit        Exits v6502.\n"
-			   "!run         Contunuously steps the cpu until a 'brk' instruction is encountered.\n"
-			   "!reset       Resets the CPU.\n"
-			   "!mreset      Zeroes all memory.\n"
-			   "!step        Forcibly steps the CPU once.\n"
-			   "!v           Toggle verbose mode; prints each instruction as they are executed when running.\n"
-			   "Anything not starting with an exclamation point is interpreted as a assembly instruction.\n");
-		return 0;
+	if (compareCommand(command, "help")) {
+		printf("cpu         Displays the current state of the CPU.\n"
+			   "dis <addr>  Disassemble ten instructions starting at a given address, or the program counter if no address is specified.\n"
+			   "help        Displays this help.\n"
+			   "load <file> Load binary image into memory at 0x0600.\n"
+			   "peek <addr> Dumps the memory at and around a given address.\n"
+			   "quit        Exits v6502.\n"
+			   "run         Contunuously steps the cpu until a 'brk' instruction is encountered.\n"
+			   "reset       Resets the CPU.\n"
+			   "mreset      Zeroes all memory.\n"
+			   "step        Forcibly steps the CPU once.\n"
+			   "v           Toggle verbose mode; prints each instruction as they are executed when running.\n");
+		return YES;
 	}
-	if (!strncmp(command, "cpu", 3)) {
+	if (compareCommand(command, "cpu")) {
 		v6502_printCpuState(cpu);
-		return 0;
+		return YES;
 	}
-	if (!strncmp(command, "load", 4)) {
+	if (compareCommand(command, "load")) {
 		command = trimheadtospc(command);
 		
 		if (command[0]) {
 			command++;
 		}
 		else {
-			return 0;
+			return YES;
 		}
 		
 		trimtaild(command);
 		loadProgram(cpu->memory, command);
-		return 0;
+		return YES;
 	}
-	if (!strncmp(command, "dis", 3)) {
+	if (compareCommand(command, "dis")) {
 		command = trimheadtospc(command);
 		
 		if (command[0]) {
 			command++;
 		}
 		else {
-			return 0;
+			return YES;
 		}
 		
 		uint8_t high, low;
@@ -161,21 +170,21 @@ static int handleDebugCommand(v6502_cpu *cpu, char *command) {
 			start += printSingleInstruction(cpu, start);
 		}
 		
-		return 0;
+		return YES;
 	}
-	if (!strncmp(command, "step", 4)) {
+	if (compareCommand(command, "step")) {
 		printSingleInstruction(cpu, cpu->pc);
 		v6502_step(cpu);
-		return 0;
+		return 1;
 	}
-	if (!strncmp(command, "peek", 4)) {
+	if (compareCommand(command, "peek")) {
 		command = trimheadtospc(command);
 		
 		if (command[0]) {
 			command++;
 		}
 		else {
-			return 0;
+			return YES;
 		}
 		
 		// Make sure we don't go out of bounds either direction
@@ -196,28 +205,31 @@ static int handleDebugCommand(v6502_cpu *cpu, char *command) {
 		v6502_printMemoryRange(cpu->memory, start, 0x30);
 		return 0;
 	}
-	if (!strncmp(command, "quit", 4)) {
-		return 1;
+	if (compareCommand(command, "quit")) {
+		v6502_destroyMemory(cpu->memory);
+		v6502_destroyCPU(cpu);
+		
+		exit(EXIT_SUCCESS);
+		return NO;
 	}
-	if (!strncmp(command, "run", 3)) {
+	if (compareCommand(command, "run")) {
 		run(cpu);
-		return 0;
+		return YES;
 	}
-	if (!strncmp(command, "reset", 5)) {
+	if (compareCommand(command, "reset")) {
 		v6502_reset(cpu);
-		return 0;
+		return YES;
 	}
-	if (!strncmp(command, "mreset", 5)) {
+	if (compareCommand(command, "mreset")) {
 		bzero(cpu->memory->bytes, cpu->memory->size * sizeof(uint8_t));
-		return 0;
+		return YES;
 	}
-	if (!strncmp(command, "v", 1)) {
+	if (compareCommand(command, "v")) {
 		printf("Verbose mode %s.\n", verbose ? "disabled" : "enabled");
 		verbose ^= 1;
-		return 0;
+		return YES;
 	}
-	printf("Unknown Command - %s\n", command);
-	return 0;
+	return NO;
 }
 
 void handleSignal(int signal) {
@@ -263,17 +275,14 @@ int main(int argc, const char * argv[])
 		printf("(0x%04x) ", cpu->pc);
 		fflush(stdout);
 		
-		// getInput(command, MAX_COMMAND_LEN);
 		fgets(command, MAX_COMMAND_LEN, stdin);
 		
 		if (command[0] == '\n') {
 			continue;
 		}
 
-		if (command[0] == '!') {
-			if (handleDebugCommand(cpu, command + 1)) {
-				break;
-			}
+		if (handleDebugCommand(cpu, command)) {
+			continue;
 		}
 		else if (command[0] != ';') {
 			as6502_executeAsmLineOnCPU(cpu, command, strlen(command));
