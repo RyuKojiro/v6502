6ecd35eaba11c1da35758a317d2bf7082a9aa7e2
Daniel Loffgren <daniel@loffgren.org>
2013-09-11 08:50:29 +0000

    Builds a symbol table now, but does not successfully determine addresses of symbols and print them to the file properly.
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@456 96dfbd92-d197-e211-9ca9-00110a534b34

--- dis6502/main.c
+++ dis6502/main.c
@@ -12,22 +12,76 @@
 #include "error.h"
 #include "flat.h"
 #include "reverse.h"
+#include "symbols.h"
 
-#define MAX_LINE_LEN		80
 #define MAX_FILENAME_LEN	255
+#define MAX_LINE_LEN		80
+#define MAX_SYM_LEN			25
+
+static int _isBranchOpcode(v6502_opcode opcode) {
+	switch (opcode) {
+		case v6502_opcode_bcc:
+		case v6502_opcode_bcs:
+		case v6502_opcode_beq:
+		case v6502_opcode_bmi:
+		case v6502_opcode_bne:
+		case v6502_opcode_bpl:
+		case v6502_opcode_bvc:
+		case v6502_opcode_bvs:
+		case v6502_opcode_jmp_abs:
+		case v6502_opcode_jmp_ind:
+		case v6502_opcode_jsr:
+			return YES;
+		default:
+			return NO;
+	}
+}
 
 static void disassembleFile(FILE *in, FILE *out) {
 	char line[MAX_LINE_LEN];
+	char symbolName[MAX_SYM_LEN];
+	int currentLabel = 1;
+	uint16_t address;
 	
 	as6502_object *obj = as6502_createObject();
 	as6502_readObjectFromFlatFile(obj, in);
 	
+	as6502_symbol_table *table = as6502_createSymbolTable();
 	
-	as6502_object_blob blob = obj->blobs[0];
-	for (uint8_t offset = 0; offset < blob.len; offset++) {
-		as6502_stringForInstruction(line, MAX_LINE_LEN, blob.data[0], blob.data[2], blob.data[1]);
-		offset += v6502_instructionLengthForOpcode(blob.data[0]);
-		fprintf(out, "%s\n", line);
+	for (int i = 0; i < obj->count; i++) {
+		as6502_object_blob *blob = &obj->blobs[i];
+
+		// Build Symbol Table
+		currentLineNum = 0;
+		for (uint8_t offset = 0; offset < blob->len; offset += v6502_instructionLengthForOpcode(blob->data[offset])) {
+			v6502_opcode opcode = blob->data[offset];
+			if (_isBranchOpcode(opcode)) {
+				snprintf(symbolName, MAX_SYM_LEN, "Label%d", currentLabel++);
+				
+				if (opcode == v6502_opcode_jmp_abs || opcode == v6502_opcode_jmp_ind || opcode == v6502_opcode_jsr) {
+					address = (blob->data[offset + 2] << 8 | blob->data[offset + 1]);
+				}
+				else {
+					address = 0x600 + offset + blob->data[offset + 1];
+				}
+				
+				as6502_addSymbolToTable(table, currentLineNum, symbolName, address, as6502_symbol_type_label);
+			}
+			currentLineNum++;
+		}
+
+		// Disassemble
+		currentLineNum = 0;
+		for (uint8_t offset = 0; offset < blob->len; offset += v6502_instructionLengthForOpcode(blob->data[offset])) {
+			as6502_symbol *label = as6502_symbolForAddress(table, offset - 0x600);
+			if (label) {
+				fprintf(out, "%s:\n", label->name);
+			}
+			
+			as6502_stringForInstruction(line, MAX_LINE_LEN, blob->data[offset], blob->data[offset + 2], blob->data[offset + 1]);
+			fprintf(out, "%s\n", line);
+			currentLineNum++;
+		}
 	}
 	
 	as6502_destroyObject(obj);
