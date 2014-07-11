867f514603cd113bfe9e8a1c8432aad71549455b
Daniel Loffgren <daniel@loffgren.org>
2014-07-11 07:19:27 +0000

    Documented breakpoint header for doxygen
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@564 96dfbd92-d197-e211-9ca9-00110a534b34

--- v6502/breakpoint.h
+++ v6502/breakpoint.h
@@ -1,3 +1,6 @@
+/** @brief Breakpoint Manangement */
+/** @file breakpoint.h */
+
 /*
  * Copyright (c) 2013 Daniel Loffgren
  *
@@ -23,15 +26,26 @@
 #ifndef v6502_breakpoint_h
 #define v6502_breakpoint_h
 
+/** @defgroup breakpoint Breakpoint Management */
+/**@{*/
+/** @brief Breakpoint List Object */
 typedef struct {
+	/** @brief Array of breakpoint addresses */
 	uint16_t *breakpoints;
+	/** @brief Number of breakpoints in the array */
 	size_t count;
 } v6502_breakpoint_list;
 
+/** @brief Create a v6502_breakpoint_list */
 v6502_breakpoint_list *v6502_createBreakpointList(void);
+/** @brief Destroy a v6502_breakpoint_list */
 void v6502_destroyBreakpointList(v6502_breakpoint_list *list);
+/** @brief Insert a breakpoint at the end of the specified v6502_breakpoint_list */
 void v6502_addBreakpointToList(v6502_breakpoint_list *list, uint16_t address);
+/** @brief Locate and remove the first matching address in a v6502_breakpoint_list */
 void v6502_removeBreakpointFromList(v6502_breakpoint_list *list, uint16_t address);
+/** @brief Check if an address is present in a v6502_breakpoint_list */
 int v6502_breakpointIsInList(v6502_breakpoint_list *list, uint16_t address);
+/**@}*/
 
 #endif
