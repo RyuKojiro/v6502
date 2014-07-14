90532823ba06e3921a15f6ef23951601037e2cb6
Daniel Loffgren <daniel@loffgren.org>
2014-07-14 18:54:05 +0000

    Adding preliminary text mode video code
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@580 96dfbd92-d197-e211-9ca9-00110a534b34

---
+++ v6502/textmode.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2013 Daniel Loffgren
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef v6502_textmode_h
+#define v6502_textmode_h
+
+#include "memory.h"
+
+typedef struct {
+	
+} v6502_textmode_video;
+
+v6502_textmode_video *textMode_create();
+void textMode_destroy(v6502_textmode_video *vid);
+void textMode_refreshVideo(v6502_memory *mem);
+void textMode_updateCharacter(v6502_memory *mem, int x, int y);
+
+#endif
