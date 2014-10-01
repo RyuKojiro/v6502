73e2d0e205f02ecc53eea721706f432c10a8c0f5
Daniel Loffgren <daniel@loffgren.org>
2014-10-01 21:08:58 +0000

    Added textmode doxygen
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@681 96dfbd92-d197-e211-9ca9-00110a534b34

--- v6502/textmode.h
+++ v6502/textmode.h
@@ -1,3 +1,6 @@
+/** @brief 6502 Reference Platform Virtual Terminal */
+/** @file textmode.h */
+
 /*
  * Copyright (c) 2013 Daniel Loffgren
  *
@@ -27,8 +30,11 @@
 
 #include "mem.h"
 
+/** @brief The start address of character data in memory */
 #define textMode_characterMemoryStart	0x2000
+/** @brief The start address of attribute data in memory */
 #define textMode_attributeMemoryStart	0x3000
+/** @brief The upper bounds of memory reserved for terminal hardware */
 #define textMode_memoryCeiling			0x4000
 
 /** The first two kilobytes of memory (starting at textMode_characterMemoryStart or 0x2000) are character data, starting with the top right, ending with the bottom left, one row at a time, with no interruptions. After a short break for video hardware registers, another 2 kilobytes (starting at textMode_attributeMemoryStart or 0x3000) are attribute data, which correspond to each byte of character data + 0x1000.
@@ -39,16 +45,28 @@
 	B C C C  B C C C
  */
 
+/** @defgroup textmode Reference Platform Video/Keyboard */
+/**@{*/
+/** @struct */
+/** @brief Virtual Text-Mode Hardware Object */
 typedef struct {
+	/** @brief Curses output object */
 	WINDOW *screen;
+	/** @brief Hardwired memory used to trap video activity and report keyboard input */
 	v6502_memory *memory;
 } v6502_textmode_video;
 
+/** @brief Create v6502_textmode_video */
 v6502_textmode_video *textMode_create(v6502_memory *mem);
+/** @brief Destroy v6502_textmode_video */
 void textMode_destroy(v6502_textmode_video *vid);
+/** @brief Put virtual video into "rest" mode, where the hosting terminal is restored, but the video data is preserved and will be redisplayed on next access */
 void textMode_rest(v6502_textmode_video *vid);
+/** @brief Force a fullscreen refresh */
 void textMode_refreshVideo(v6502_textmode_video *vid);
+/** @brief Refresh a single character of the output (When memory is trapped, this is called automatically on memory interaction) */
 void textMode_updateCharacter(v6502_textmode_video *vid, int x, int y);
+/** @brief Convert x, y coordinates to the address in memory that is expected to hold the character */
 uint16_t textMode_addressForLocation(int x, int y);
 
 #endif
