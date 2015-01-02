7d6d5b784c25940244822b08db0e16a8daad255a
Daniel Loffgren <daniel@loffgren.org>
2015-01-02 06:38:30 +0000

    Moved the actual lib building targets to their respective Makefiles, and just use make -C to get to the actual target makes
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@722 96dfbd92-d197-e211-9ca9-00110a534b34

--- libtargets.mk
+++ libtargets.mk
@@ -1,11 +1,11 @@
-$(LIBAS6502): $(LIBAS6502_OBJS)
-	$(AR) $(ARFLAGS) $@ $(LIBAS6502_OBJS)
+$(LIBAS6502):
+	make -C ../as6502/ lib
 
-$(LIBDIS6502): $(LIBDIS6502_OBJS)
-	$(AR) $(ARFLAGS) $@ $(LIBDIS6502_OBJS)
+$(LIBDIS6502):
+	make -C ../dis6502/ lib
 
-$(LIBLD6502): $(LIBLD6502_OBJS)
-	$(AR) $(ARFLAGS) $@ $(LIBLD6502_OBJS)
+$(LIBLD6502):
+	make -C ../ld6502/ lib
 
-$(LIBV6502): $(LIBV6502_OBJS)
-	$(AR) $(ARFLAGS) $@ $(LIBV6502_OBJS)
+$(LIBV6502):
+	make -C ../v6502/ lib
