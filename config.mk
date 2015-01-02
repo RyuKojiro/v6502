8ffe5207c62902d7df9801fdd7e701ba4e7cc59d
Daniel Loffgren <daniel@loffgren.org>
2015-01-02 03:37:18 +0000

    New lib top level mk files that are included in bin targets for building and have fancy vars for each library, fully working example in v6502's makefile
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/branches/library_build@715 96dfbd92-d197-e211-9ca9-00110a534b34

--- config.mk
+++ config.mk
@@ -1,2 +1,2 @@
 INCLUDE+=       -I../as6502/ -I../v6502/ -I../dis6502/ -I../ld6502/
-CFLAGS+=	-Wall -Wno-unknown-pragmas -std=c99 -ggdb $(INCLUDE)
+CFLAGS+=		-Wall -Wno-unknown-pragmas -std=c99 -ggdb $(INCLUDE)
