c5124e861dad8280e5794272f9623abdf63f1593
Daniel Loffgren <daniel@loffgren.org>
2015-08-24 00:47:33 +0000

    The start of a basic installation target for as6502, all of the includes need to change to be prefixed with the module
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@908 96dfbd92-d197-e211-9ca9-00110a534b34

--- config.mk
+++ config.mk
@@ -1,2 +1,12 @@
+# Compilation
 INCLUDE+=       -I../as6502/ -I../v6502/ -I../dis6502/ -I../ld6502/
 CFLAGS+=		-Wall -Wno-unknown-pragmas -std=c99 -ggdb $(INCLUDE) -D_XOPEN_SOURCE
+
+# Installation
+DESTDIR?=		/
+PREFIX?=		/usr/local/
+
+MANDEST=		$(DESTDIR)/$(PREFIX)/man/man1/
+BINDEST=		$(DESTDIR)/$(PREFIX)/bin/
+LIBDEST=		$(DESTDIR)/$(PREFIX)/lib/
+INCDEST=		$(DESTDIR)/$(PREFIX)/include/
