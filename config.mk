2ba3357c7fd36ba08378fb7b97c6a2e45e4cea87
Daniel Loffgren <daniel@loffgren.org>
2015-08-24 01:20:42 +0000

    Fixed man path for installation to be /usr/share/man/man1, since /usr/local/man/* is not used
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/branches/install_target@921 96dfbd92-d197-e211-9ca9-00110a534b34

--- config.mk
+++ config.mk
@@ -6,7 +6,7 @@ CFLAGS+=		-Wall -Wno-unknown-pragmas -std=c99 -ggdb $(INCLUDE) -D_XOPEN_SOURCE
 DESTDIR?=		/
 PREFIX?=		/usr/local/
 
-MANDEST=		$(DESTDIR)/$(PREFIX)/man/man1/
+MANDEST=		$(DESTDIR)/usr/share/man/man1/
 BINDEST=		$(DESTDIR)/$(PREFIX)/bin/
 LIBDEST=		$(DESTDIR)/$(PREFIX)/lib/
 INCDEST=		$(DESTDIR)/$(PREFIX)/include/
