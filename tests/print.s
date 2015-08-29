70230eda6b72d94348fc9d6332e402ca2f6ed564
Daniel Loffgren <daniel@loffgren.org>
2015-08-29 07:11:37 +0000

    Some work-in-progress scrolling changes
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@975 96dfbd92-d197-e211-9ca9-00110a534b34

--- tests/print.s
+++ tests/print.s
@@ -23,27 +23,41 @@
 ;
 
 
-	lda #$1f
+	lda #$0d
 	ldy #$06
 	jsr PRINT
 end:				; This loop is effectively a WAI instruction
 	jmp end
 	jmp string			; This only exists to emit the string label address
 
-;; PRINT ;; line print the string located at $YYAA
-PRINT:
-	sta *$00			; create a pointer in *$01,00 that contains the string start
-	sty *$01			; the lower byte of the target string
-	ldy #$00			; Reset the Y register to zero, since it is next used ¬
-							; as the iterator over the strings
-nextChar:			; This is basically a strcpy loop
-	lda ($00),Y			; load the byte (Y is required for this kind of indirection)
-	sta $2000,Y			; store the byte in the same offset at the target location
-	iny					; increment the byte offset
-	beq donePrinting	; If the next byte is null, we're done
-	jmp nextChar		; otherwise, rinse and repeat
-donePrinting:		; we hit a null byte
-	rts					; return
-
 string:
 .asciiz "This is a line test"
+
+;;; PRINT ;;; line print the string located at $YYAA
+			; This requires the use of *00,*01 for temporary string pointer
+			; storage, it rotates lines up if it encounters a newline (0x00)
+PRINT:
+	sta *$00			; create a pointer in *$01,00 that contains the string start
+	sty *$01			; the lower byte of the origin string
+
+	lda #$00			; Create a pointer in $YYAA that contains the console ¬
+	ldy #$20				; output target
+	sta *$02			; put the pointer into *$03,02
+	sty *$03			; the lower byte of the destination string
+
+	ldy #$00			; Reset the Y register to zero, since it is next used ¬
+							; as the iterator over the strings
+strcpy:			; Works like normal strcpy, expects the output target in *$03,02
+	lda ($00),Y			; load the byte (Y is required for this kind of indirection)
+	sta ($02),Y			; store the byte in the same offset at the target location
+	iny					; increment the byte offset
+	beq doneCopying	; If the next byte is null, we're done
+	jmp strcpy		; otherwise, rinse and repeat
+doneCopying:		; we hit a null byte
+	rts					; return
+
+;;; SCROLL ;; bumps the scrollback up one line
+
+SCROLL:
+	ldy #$00		; reset Y register, since we are going to use it to iterate
+nextLine:
