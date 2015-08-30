b19f33de50616fcdfa226ea075ad2ca064904a7a
Daniel Loffgren <daniel@loffgren.org>
2015-08-30 09:05:06 +0000

    Added address modes to the kmap generator
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@983 96dfbd92-d197-e211-9ca9-00110a534b34

--- kmapgen/main.c
+++ kmapgen/main.c
@@ -11,27 +11,94 @@
 #include <dis6502/reverse.h>
 
 #define OPCODE_STRING_LEN	4
+#define MODE_STRING_LEN		6
+
+void _shortStringForAddressMode(char *out, size_t len, v6502_address_mode mode) {
+	switch (mode) {
+		case v6502_address_mode_accumulator: {
+			strncpy(out, "A", len);
+		} return;
+		case v6502_address_mode_implied: {
+			strncpy(out, "imp", len);
+		} return;
+		case v6502_address_mode_zeropage: {
+			strncpy(out, "zpg", len);
+		} return;
+		case v6502_address_mode_indirect: {
+			strncpy(out, "ind", len);
+		} return;
+		case v6502_address_mode_relative: {
+			strncpy(out, "rel", len);
+		} return;
+		case v6502_address_mode_immediate: {
+			strncpy(out, "#", len);
+		} return;
+		case v6502_address_mode_zeropage_x: {
+			strncpy(out, "zpg,X", len);
+		} return;
+		case v6502_address_mode_zeropage_y: {
+			strncpy(out, "zpg,Y", len);
+		} return;
+		case v6502_address_mode_absolute: {
+			strncpy(out, "abs", len);
+		} return;
+		case v6502_address_mode_absolute_x: {
+			strncpy(out, "abs,X", len);
+		} return;
+		case v6502_address_mode_absolute_y: {
+			strncpy(out, "abs,Y", len);
+		} return;
+		case v6502_address_mode_indirect_x: {
+			strncpy(out, "X,ind", len);
+		} return;
+		case v6502_address_mode_indirect_y: {
+			strncpy(out, "ind,Y", len);
+		} return;
+		case v6502_address_mode_symbol: {
+			strncpy(out, "sym", len);
+		} return;
+		case v6502_address_mode_unknown:
+		default:
+			out[0] = '\0';
+			break;
+	}
+}
 
 void generateTable(FILE *out) {
 	char opcodeString[OPCODE_STRING_LEN];
+	char modeString[MODE_STRING_LEN];
 	
-	fprintf(out, "<html><body>\n");
 	fprintf(out, "<table border=\"1\" cellspacing=\"0\" cellpadding=\"5\" class=\"opctable\">\n");
 	for (uint8_t high = 0; high < 0x10; high++) {
 		fprintf(out, "<tr valign=\"top\"><td nowrap=\"\">%2x</td>", high << 4);
 		for (uint8_t low = 0; low < 0x10; low++) {
 			uint8_t opcode = ((high << 4) | low);
+			v6502_address_mode mode = v6502_addressModeForOpcode(opcode);
+
 			dis6502_stringForOpcode(opcodeString, OPCODE_STRING_LEN, opcode);
-			fprintf(out, "<td nowrap>%s</td>", opcodeString);
+			_shortStringForAddressMode(modeString, MODE_STRING_LEN, mode);
+			
+			fprintf(out, "<td nowrap>%s %s</td>", opcodeString, modeString);
 		}
 		fprintf(out, "</tr>\n");
 	}
 	fprintf(out, "</table>");
+}
+
+void generateAllTables(FILE *out) {
+	fprintf(out, "<html><body>\n");
+	
+	// Address Mode
+	generateTable(out);
+	
+	// Instruction Length
+	
+	// Opcode
+	
 	fprintf(out, "</html></body>\n");
 }
 
-
 int main(int argc, const char * argv[]) {
-	generateTable(stdout);
+	generateAllTables(stdout);
     return 0;
 }
