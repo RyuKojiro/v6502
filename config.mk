8f595844600fb7ced7695894ceb63a0f03e4b038
Daniel Loffgren <daniel@loffgren.org>
2016-12-05 05:26:42 +0000

    Reverted the _XOPEN_SOURCE=700 change in favor of adding _GNU_SOURCE, and broke them up to give comments on why each exists
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/trunk@1184 96dfbd92-d197-e211-9ca9-00110a534b34

--- config.mk
+++ config.mk
@@ -1,6 +1,9 @@
 # Compilation
 INCLUDE+=       -I..
-CFLAGS+=		-Wall -Wno-unknown-pragmas -std=c99 $(INCLUDE) -D_XOPEN_SOURCE=700
+CFLAGS+=		-Wall -Wno-unknown-pragmas -std=c99 $(INCLUDE)
+CFLAGS+=		-D_XOPEN_SOURCE # For getopt
+CFLAGS+=		-D_XOPEN_SOURCE_EXTENDED # For strndup
+CFLAGS+=		-D_GNU_SOURCE # For getline without breaking SIGWINCH (_POSIX_C_SOURCE will break SIGWINCH on many systems)
 #CFLAGS+=		-ggdb -O0
 
 # Installation
