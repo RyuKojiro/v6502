9522bde620c4738904c87f83dc6aeec36a8a8f64
Daniel Loffgren <daniel@loffgren.org>
2017-02-10 08:50:36 +0000

    Match everything, even if it has an address mode, store the address mode, and store the hex
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/branches/isa_dox@1209 96dfbd92-d197-e211-9ca9-00110a534b34

--- isagen.pl
+++ isagen.pl
@@ -8,8 +8,8 @@ my $cpu_source = "v6502/cpu.h";
 open my $f, $cpu_source or die "Unable to read from cpu header";
 
 while (my $line = <$f>) {
-	if($line =~ /v6502_opcode_([[:alpha:]]+)/) {
-		print $1 . "\n";
+	if($line =~ /v6502_opcode_([[:alpha:]]+)_?([[:alpha:]]*)\s*=\s*(0x[[:xdigit:]]{2})/) {
+		print $1 . " > " . $3 . "\n";
 	}
 }
 
