2b560ee096130bfc84e53bb579e727d82574c038
Daniel Loffgren <daniel@loffgren.org>
2017-02-10 09:18:20 +0000

    Mode recognition
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/branches/isa_dox@1210 96dfbd92-d197-e211-9ca9-00110a534b34

--- isagen.pl
+++ isagen.pl
@@ -8,8 +8,16 @@ my $cpu_source = "v6502/cpu.h";
 open my $f, $cpu_source or die "Unable to read from cpu header";
 
 while (my $line = <$f>) {
-	if($line =~ /v6502_opcode_([[:alpha:]]+)_?([[:alpha:]]*)\s*=\s*(0x[[:xdigit:]]{2})/) {
-		print $1 . " > " . $3 . "\n";
+	if($line =~ /v6502_opcode_([[:alpha:]]+)_?([[:alpha:]]*)\s*=\s*(0x[[:xdigit:]]{2}),\s*\/{0,2}(.*)/) {
+		my $mode = "imp";
+		if($2) {
+			$mode = $2;
+		}
+		print "$1, $mode, $3";
+		if ($4) {
+			print " >> $4";
+		}
+		print "\n";
 	}
 }
 
