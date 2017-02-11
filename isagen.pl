64817c83902c4363f8a6191b34a932ea2e5a114b
Daniel Loffgren <daniel@loffgren.org>
2017-02-11 09:49:41 +0000

    Preserve previous comment lines for blocks of address mode variations
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/branches/isa_dox@1216 96dfbd92-d197-e211-9ca9-00110a534b34

--- isagen.pl
+++ isagen.pl
@@ -9,20 +9,28 @@ my %instructions;
 my $cpu_source = "v6502/cpu.h";
 open my $f, $cpu_source or die "Unable to read from cpu header";
 
+my $comment;
 while (my $line = <$f>) {
 	if($line =~ /v6502_opcode_([[:alpha:]]+)_?([[:alpha:]]*)\s*=\s*(0x[[:xdigit:]]{2}),\s*\/{0,2}\s+(.*)/) {
 		my $nmemonic = $1;
 		my $opcode = $3;
-		my $comment = $4;
 		my $addressmode = "imp";
 		if($2) {
 			$addressmode = $2;
 		}
-		print "$nmemonic, $opcode, $addressmode, $comment\n";
+		if($4) {
+			$comment = $4;
+		}
 
 		$instructions{$nmemonic}{$addressmode} = $opcode;
 		if($comment) {
 			$instructions{$nmemonic}{'comment'} = $comment;
+		}
+		undef $comment;
+	}
+	else {
+		if ($line =~ /\/\/\s+(.*)/) {
+			$comment = $1;
 		}
 	}
 }
