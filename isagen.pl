8e8841a4939af9ceae3c14fcd8ffd883e2a910db
Daniel Loffgren <daniel@loffgren.org>
2017-02-11 08:58:56 +0000

    Also eat the whitespace between the double slash and comment start
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/v6502/branches/isa_dox@1212 96dfbd92-d197-e211-9ca9-00110a534b34

--- isagen.pl
+++ isagen.pl
@@ -8,7 +8,7 @@ my $cpu_source = "v6502/cpu.h";
 open my $f, $cpu_source or die "Unable to read from cpu header";
 
 while (my $line = <$f>) {
-	if($line =~ /v6502_opcode_([[:alpha:]]+)_?([[:alpha:]]*)\s*=\s*(0x[[:xdigit:]]{2}),\s*\/{0,2}(.*)/) {
+	if($line =~ /v6502_opcode_([[:alpha:]]+)_?([[:alpha:]]*)\s*=\s*(0x[[:xdigit:]]{2}),\s*\/{0,2}\s+(.*)/) {
 		my $mode = "imp";
 		if($2) {
 			$mode = $2;
