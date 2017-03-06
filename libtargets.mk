304c2670e431aed08253a75ece07e6d65c00679d
Daniel Loffgren <daniel@loffgren.org>
2017-03-05 19:28:52 -0800

    Use the V6502_PREFIX for libtargets, as well

--- libtargets.mk
+++ libtargets.mk
@@ -1,14 +1,14 @@
 $(LIBAS6502):
-	make -C ../as6502/ lib
+	make -C $(V6502_PREFIX)/as6502/ lib
 
 $(LIBDIS6502):
-	make -C ../dis6502/ lib
+	make -C $(V6502_PREFIX)/dis6502/ lib
 
 $(LIBLD6502):
-	make -C ../ld6502/ lib
+	make -C $(V6502_PREFIX)/ld6502/ lib
 
 $(LIBV6502):
-	make -C ../v6502/ lib
+	make -C $(V6502_PREFIX)/v6502/ lib
 
 analyze: $(SRCS) $(LIBSRCS)
 	$(CC) $(INCLUDE) --analyze -Weverything $(CFLAGS) $(SRCS) $(LIBSRCS)
